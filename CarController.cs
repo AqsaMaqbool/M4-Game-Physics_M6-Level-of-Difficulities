e44cd45679f38df906278ba74a35f8854fe516fb
AqsaMaqbool <58320532+AqsaMaqbool@users.noreply.github.com>
2020-08-25 21:26:00 +0500

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,162 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    internal enum CarDriveType
+    {
+        FrontWheelDrive,
+        RearWheelDrive,
+        FourWheelDrive
+    }
+
+    internal enum SpeedType
+    {
+        MPH,
+        KPH
+    }
+
+    public class CarController : MonoBehaviour
+    {
+        [SerializeField] private CarDriveType m_CarDriveType = CarDriveType.FourWheelDrive;
+        [SerializeField] private WheelCollider[] m_WheelColliders = new WheelCollider[4];
+        [SerializeField] private GameObject[] m_WheelMeshes = new GameObject[4];
+        [SerializeField] private WheelEffects[] m_WheelEffects = new WheelEffects[4];
+        [SerializeField] private Vector3 m_CentreOfMassOffset;
+        [SerializeField] private float m_MaximumSteerAngle;
+        [Range(0, 1)] [SerializeField] private float m_SteerHelper; // 0 is raw physics , 1 the car will grip in the direction it is facing
+        [Range(0, 1)] [SerializeField] private float m_TractionControl; // 0 is no traction control, 1 is full interference
+        [SerializeField] private float m_FullTorqueOverAllWheels;
+        [SerializeField] private float m_ReverseTorque;
+        [SerializeField] private float m_MaxHandbrakeTorque;
+        [SerializeField] private float m_Downforce = 100f;
+        [SerializeField] private SpeedType m_SpeedType;
+        [SerializeField] private float m_Topspeed = 200;
+        [SerializeField] private static int NoOfGears = 5;
+        [SerializeField] private float m_RevRangeBoundary = 1f;
+        [SerializeField] private float m_SlipLimit;
+        [SerializeField] private float m_BrakeTorque;
+
+        private Quaternion[] m_WheelMeshLocalRotations;
+        private Vector3 m_Prevpos, m_Pos;
+        private float m_SteerAngle;
+        private int m_GearNum;
+        private float m_GearFactor;
+        private float m_OldRotation;
+        private float m_CurrentTorque;
+        private Rigidbody m_Rigidbody;
+        private const float k_ReversingThreshold = 0.01f;
+
+        public bool Skidding { get; private set; }
+        public float BrakeInput { get; private set; }
+        public float CurrentSteerAngle{ get { return m_SteerAngle; }}
+        public float CurrentSpeed{ get { return m_Rigidbody.velocity.magnitude*2.23693629f; }}
+        public float MaxSpeed{get { return m_Topspeed; }}
+        public float Revs { get; private set; }
+        public float AccelInput { get; private set; }
+
+
+
+        // this is used to add more grip in relation to speed
+        private void AddDownForce()
+        {
+            m_WheelColliders[0].attachedRigidbody.AddForce(-transform.up*m_Downforce*
+                                                         m_WheelColliders[0].attachedRigidbody.velocity.magnitude);
+        }
+
+
+    
+        private void CheckForWheelSpin()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                WheelHit wheelHit;
+                m_WheelColliders[i].GetGroundHit(out wheelHit);
+
+                // is the tire slipping above the given threshhold
+                if (Mathf.Abs(wheelHit.forwardSlip) >= m_SlipLimit || Mathf.Abs(wheelHit.sidewaysSlip) >= m_SlipLimit)
+                {
+                    m_WheelEffects[i].EmitTyreSmoke();
+
+                
+                    if (!AnySkidSoundPlaying())
+                    {
+                        m_WheelEffects[i].PlayAudio();
+                    }
+                    continue;
+                }
+
+                // if it wasnt slipping stop all the audio
+                if (m_WheelEffects[i].PlayingAudio)
+                {
+                    m_WheelEffects[i].StopAudio();
+                }
+                // end the trail generation
+                m_WheelEffects[i].EndSkidTrail();
+            }
+        }
+
+        private void TractionControl()
+        {
+            WheelHit wheelHit;
+            switch (m_CarDriveType)
+            {
+                case CarDriveType.FourWheelDrive:
+                    // loop through all wheels
+                    for (int i = 0; i < 4; i++)
+                    {
+                        m_WheelColliders[i].GetGroundHit(out wheelHit);
+
+                        AdjustTorque(wheelHit.forwardSlip);
+                    }
+                    break;
+
+                case CarDriveType.RearWheelDrive:
+                    m_WheelColliders[2].GetGroundHit(out wheelHit);
+                    AdjustTorque(wheelHit.forwardSlip);
+
+                    m_WheelColliders[3].GetGroundHit(out wheelHit);
+                    AdjustTorque(wheelHit.forwardSlip);
+                    break;
+
+                case CarDriveType.FrontWheelDrive:
+                    m_WheelColliders[0].GetGroundHit(out wheelHit);
+                    AdjustTorque(wheelHit.forwardSlip);
+
+                    m_WheelColliders[1].GetGroundHit(out wheelHit);
+                    AdjustTorque(wheelHit.forwardSlip);
+                    break;
+            }
+        }
+
+
+        private void AdjustTorque(float forwardSlip)
+        {
+            if (forwardSlip >= m_SlipLimit && m_CurrentTorque >= 0)
+            {
+                m_CurrentTorque -= 10 * m_TractionControl;
+            }
+            else
+            {
+                m_CurrentTorque += 10 * m_TractionControl;
+                if (m_CurrentTorque > m_FullTorqueOverAllWheels)
+                {
+                    m_CurrentTorque = m_FullTorqueOverAllWheels;
+                }
+            }
+        }
+
+
+        private bool AnySkidSoundPlaying()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (m_WheelEffects[i].PlayingAudio)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
